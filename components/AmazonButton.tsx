fe1ea427a028565b858f989f589ec934727b6e5f
programacion3 <141765274+programacion3@users.noreply.github.com>
2026-05-14 10:50:48 -0600

    regresar cierra chrome6

--- components/AmazonButton.tsx
+++ components/AmazonButton.tsx
@@ -1,47 +1,24 @@
-"use client"
-
-import { generateAmazonLink }
-from "@/lib/amazon"
+import { generateAmazonLink } from "@/lib/amazon"
 
 export default function AmazonButton({
-  amazon
+  amazon,
 }: {
   amazon: Record<string, string>
 }) {
+  let country = "US"
+  const lang = navigator.language.toLowerCase()
 
-  const handleClick = () => {
-
-
-    //PARA PROBAR COMENTAMOS DESDE ACÁ
-    let country = "US"
-    const lang =
-      navigator.language.toLowerCase()
-
-    if (
-      lang.includes("es-es")
-    ) {
-      country = "ES"
-    } // Y HASTA ACÁ, Y LUEGO
-
-    //DESCOMENTAMOS ESTA PARTECITA
-    /*const TEST_COUNTRY = "US"
-    let country = TEST_COUNTRY*/
-
-
-    
-
-    const url =
-      generateAmazonLink(
-        amazon,
-        country
-      )
-
-    window.open(url, "_blank")
+  if (lang.includes("es-es")) {
+    country = "ES"
   }
 
+  const url = generateAmazonLink(amazon, country)
+
   return (
-    <button 
-      onClick={handleClick}
+    <a
+      href={url}
+      target="_self"
+      rel="noopener noreferrer"
       className="
         mt-4
         bg-yellow-500
@@ -52,10 +29,11 @@ export default function AmazonButton({
         font-medium
         text-sm
         transition
+        inline-block
         whitespace-nowrap
       "
     >
       Ver en Amazon
-    </button>
+    </a>
   )
 }
