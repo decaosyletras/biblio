1b6f897281f59e74ea1a58fecd5927a361858eee
Marcos lap <marcos48151@gmail.com>
2026-05-14 21:00:34 -0600

    pruebas

--- app/libros/page.tsx
+++ app/libros/page.tsx
@@ -66,7 +66,7 @@ export default function Page() {
           <BookRow
             title=""
             books={books}
-            noShuffle={false}
+            noShuffle={true}
           />
 
         </section>
