5dbd4fe3f500a64bdbab86baaacede9d6678f682
programacion3 <141765274+programacion3@users.noreply.github.com>
2026-07-15 10:24:03 -0600

    ediciones4.2

--- app/authors/[slug]/page.tsx
+++ app/authors/[slug]/page.tsx
@@ -684,7 +684,7 @@ export default async function AuthorPage({
                                 <div>
 
                                     <h2
-                                        className="text-xl md:text-2xl font-bold mb-8 md:mb-6"
+                                        className="text-2xl font-bold mb-6 md:mb-4"
                                         style={{
                                             color: authorTheme.text
                                         }}
