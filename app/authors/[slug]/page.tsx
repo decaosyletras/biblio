0fd5f806238314e7556050af8fc77090064a68e2
Marcos lap <marcos48151@gmail.com>
2026-07-27 21:03:14 -0600

    colores6

--- app/authors/[slug]/page.tsx
+++ app/authors/[slug]/page.tsx
@@ -242,11 +242,11 @@ export default async function AuthorPage({
 
     const authorTheme = {
         bg: themeConfig?.bg ?? "#151518",
-        surface: themeConfig?.surface ?? "#505061",
+        surface: themeConfig?.surface ?? "#69697c",
         primary: themeConfig?.primary ?? "#3762c0",
         text: themeConfig?.text ?? "#ffffff",
         muted: themeConfig?.muted ?? "#a1a1aa",
-        border: themeConfig?.border ?? "#818198",
+        border: themeConfig?.border ?? "#4545b0",
     }
 
     /*onsole.log({
