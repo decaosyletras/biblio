b0a721fcf627b21edea1841285d7dcb1a9ecfdfa
programacion3 <141765274+programacion3@users.noreply.github.com>
2026-08-11 11:17:45 -0600

    amazonbutton

--- app/authors/[slug]/page.tsx
+++ app/authors/[slug]/page.tsx
@@ -886,11 +886,7 @@ export default async function AuthorPage({
                                     <div className="mt-2 flex flex-wrap gap-3 items-center">
 
                                         <AmazonButton
-                                            amazon={{
-                                                es: featuredBook.asin_es,
-                                                mx: featuredBook.asin_mx,
-                                                us: featuredBook.asin_us
-                                            }}
+                                            amazon={featuredBook.amazon}
                                             amazonLink={featuredBook.amazonLink}
                                             color={authorTheme.primary}
                                             textColor="#ffffff"
@@ -1054,11 +1050,7 @@ export default async function AuthorPage({
                                         <div className="mt-5 space-y-3">
 
                                             <AmazonButton
-                                                amazon={{
-                                                    es: book.asin_es,
-                                                    mx: book.asin_mx,
-                                                    us: book.asin_us
-                                                }}
+                                                amazon={book.amazon}
                                                 amazonLink={book.amazonLink}
                                                 color={authorTheme.primary}
                                                 textColor="#ffffff"
